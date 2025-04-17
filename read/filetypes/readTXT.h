53657c90ae4113677e119c04d7df73a07c64e058
Andy <ydnawizard@protonmail.com>
2025-04-17 02:57:29 -0500

    new readTXT

--- read/filetypes/readTXT.h
+++ read/filetypes/readTXT.h
@@ -1,68 +1,123 @@
-#ifndef READTXT_H
-#define READTXT_H
-
-//ANDY's FILE READING C UTILS
-
-//TXT-TYPE-SPECIFIC UTILITIES//
+#ifndef READER_H
+#define READER_H
 
 
-  ///////////////////////////////////////////////////////////////
- //MAKE SURE TO INCLUDE DIRENT & STRING LIBRARIES IN MAIN FILE//
-///////////////////////////////////////////////////////////////
-
-//DEFINE THE TXT STRUCT FOR .txt FILE TYPE
-struct TXT{
+//Define TXT struct that gets passed out
+struct TXT
+{
 	char* CHARACTERS;
 	char** STRINGS;
+	char** LINES;
 };
 
-void readCharToTXT(struct TXT* txt,char character,int charCount){
+//CHAR FIELD FUNCTIONS
+void readCharToTXT(struct TXT* txt,char character,int charCount)
+{
 	txt->CHARACTERS=realloc(txt->CHARACTERS,(charCount+1)*sizeof(char));
 	txt->CHARACTERS[charCount]=character;
-	printf("%c ",txt->CHARACTERS[charCount]);
 }
 
-void readCharToString(char character,char* string,int stringCharCount){
-	string[stringCharCount]=character;
-	string=realloc(string,(stringCharCount+1)*sizeof(char));
-}
-
-void readStringToTXT(struct TXT* txt,char* string,int stringCount){
-	txt->STRINGS=(char**)realloc(txt->STRINGS,(stringCount+1)*sizeof(char*));
-	txt->STRINGS[stringCount]=malloc((strlen(string))*sizeof(char));
-	strcpy(txt->STRINGS[stringCount],string);
-	printf("%s %s %d \n",txt->STRINGS[stringCount],"string",stringCount);
-}
-
-void readTXT(struct TXT* txt,char* filePath){
-	txt=(struct TXT*)malloc(sizeof(struct TXT));
-	txt->CHARACTERS=malloc(sizeof(char*));
-	txt->STRINGS=malloc(sizeof(char*));
-	FILE* toParse=fopen(filePath,"read");
-	char* string=malloc(sizeof(char*));
+void readCharsToTXT(struct TXT* txt,FILE* file)
+{
 	int charCount=0;
-	int stringCount=0;
-	int stringCharCount=0;
 	char character;
-	while((character=fgetc(toParse))!=EOF){
-		if(character==' '){
-			readStringToTXT(txt,string,stringCount);
+	while((character=fgetc(file))!=EOF)
+	{
+		readCharToTXT(&(*txt),character,charCount);
+		charCount+=1;
+	}
+}
+
+//STRING FIELD FUNCTIONS
+void readCharToString(char** string,char character,int charCount)
+{
+	(*string)=(char*)realloc((*string),(charCount+1)*sizeof(char));
+	(*string)[charCount]=character;
+}
+
+void readStringToTXT(struct TXT* txt,char* string,int stringCount)
+{
+	txt->STRINGS=(char**)realloc(txt->STRINGS,(stringCount+1)*sizeof(char*));
+	txt->STRINGS[stringCount]=(char*)malloc(strlen(string)*sizeof(char));
+	strcpy(txt->STRINGS[stringCount],string);
+}
+
+void readStringsToTXT(struct TXT* txt,FILE* file)
+{
+	int stringCount=0;
+	int charCount=0;
+	char* string=malloc(sizeof(char));
+	char character;
+	while((character=fgetc(file))!=EOF)
+	{
+		if(character==' '||character=='\n')
+		{
+			readStringToTXT(&(*txt),string,stringCount);
+			string=(char*)realloc(string,sizeof(char));
 			memset(string,'\0',strlen(string));
 			stringCount+=1;
-			stringCharCount=0;
-		}else if(character=='\n'){
-			readStringToTXT(txt,string,stringCount);
-			memset(string,'\0',strlen(string));
-			stringCount+=1;
-			stringCharCount=0;
-		}else{
-			readCharToTXT(txt,character,charCount);
-			readCharToString(character,string,stringCharCount);
-			stringCharCount+=1;
+			charCount=0;
+		}else
+		{
+			readCharToString(&string,character,charCount);
 			charCount+=1;
 		}
 	}
 }
 
+//LINE FIELD FUNCTIONS
+
+void readCharToLine(char** line,char character,int charCount)
+{
+	(*line)=(char*)realloc((*line),(charCount+1)*sizeof(char));
+	(*line)[charCount]=character;
+}
+
+void readLineToTXT(struct TXT* txt,char* line,int lineCount)
+{
+	txt->LINES=(char**)realloc(txt->LINES,(lineCount+1)*sizeof(char*));
+	txt->LINES[lineCount]=(char*)malloc(strlen(line)*sizeof(char));
+	strcpy(txt->LINES[lineCount],line);
+}
+
+void readLinesToTXT(struct TXT* txt,FILE* file)
+{
+	int lineCount=0;
+	int charCount=0;
+	char* line=malloc(sizeof(char));
+	char character;
+	while((character=fgetc(file))!=EOF)
+	{
+		if(character=='\n')
+		{
+			readLineToTXT(&(*txt),line,lineCount);
+			memset(line,'\0',strlen(line));
+			lineCount+=1;
+			charCount=0;
+		}else
+		{
+			readCharToLine(&line,character,charCount);
+			charCount+=1;
+		}
+	}
+}
+
+
+
+//MAIN READ FUNCTION
+void readTXT(struct TXT* txt,char* filePath)
+{
+	txt->CHARACTERS=(char*)malloc(sizeof(char));
+	txt->STRINGS=(char**)malloc(sizeof(char*));
+	txt->LINES=(char**)malloc(sizeof(char*));
+	FILE* file=fopen(filePath,"read");
+	readCharsToTXT(&(*txt),file);
+	rewind(file);
+	readStringsToTXT(&(*txt),file);
+	rewind(file);
+	readLinesToTXT(&(*txt),file);
+}
+	
+
+
 #endif
-		
